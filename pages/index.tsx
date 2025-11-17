9423e8ad2a71cfcb9348d20cc9e3e6a526fdecb8
Pedro Silva <pedroxbrs@gmail.com>
2025-11-17 20:46:06 -0300

    Feat: change phrase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 
 function Home(){
-    return <h1>Bom dia amigo, você é um inimigo!</h1>
+    return <>
+        <div className="home" style={{display: "flex", justifyContent:"center", alignItems:"center", height: "100vh"}}>
+            <h1>Se me gosta, faz um café pra mim!</h1>
+        </div>
+    </>
 }
 
 export default  Home;
